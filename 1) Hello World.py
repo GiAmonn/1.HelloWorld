93125eda4656bbe6122a2192dce198ad879bdee3
Giorgi Amonashvili <109041519+GiorgiAmon@users.noreply.github.com>
2023-03-07 01:13:12 +0400

    Changes in comments

--- 1) Hello World.py
+++ 1) Hello World.py
@@ -1,8 +1,10 @@
 #Exercise 1
 
-#Creates variable called name and asks the user to give it a value
+#Creates a sting variable called name and asks the user to give it a value
 name = input("Hey, what's your name?")
 
+#Creates a sting called "greeting" and merges some of its' text with a sting called "name"
 greeting = ("Hello, " + name.title() + "!")
-#prints the text and value from gio
+
+#prints the final value of a string called "greeting"
 print(greeting)

797af565d92f8c6de440aa1f7fd670154fe85e43
Giorgi Amonashvili <109041519+GiorgiAmon@users.noreply.github.com>
2023-03-07 01:02:18 +0400

    Update 3) HelloX.py

--- 3) HelloX.py
+++ 3) HelloX.py
@@ -1,4 +1,5 @@
 #Exercise 3: Greets you based on your name.
+print("Please choose names starting on: G, M, E or A ")
 name = input("Hey, what's your name? ")
 match name[0]:
     case "g":
@@ -7,7 +8,7 @@ match name[0]:
         print("Hello, " + name.title() + "! You look amazing today!")
     case "a":
         print("Hello, " + name.title() + "! Do you even lift bro?")
-    case "t":
+    case "a":
         print("Hello, " + name.title() + "! Wanna go for lunch?")
     case _:
         print("No special greeting for you " + name.title())

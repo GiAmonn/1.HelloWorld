38a82f69e4ccb273fea9d494975a9819f17e9499
Giorgi Amonashvili <109041519+GiorgiAmon@users.noreply.github.com>
2023-03-09 00:40:02 +0400

    initial release

---
+++ 2.6-fortytwo.py
@@ -0,0 +1,6 @@
+m = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+if "forty" and "two" in m  or "42" in m or"Forty" and "Two" in m:
+    print("yes")
+else:
+    print("no")
+    

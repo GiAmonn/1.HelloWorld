8064c95c8998f5780e089d925e80fe7f26767de7
Giorgi Amonashvili <109041519+GiorgiAmon@users.noreply.github.com>
2023-03-14 21:09:39 +0400

    Delete 2.13 camelCase.py

--- 2.13 camelCase.py
+++
@@ -1,9 +0,0 @@
-var = input("Write in camelCase: ")
-snake_case = ""
-for char in var:
-    if char.isupper():
-        snake_case += "_" + char.lower()
-    else:
-        snake_case += char
-
-print("Here's your snake_case:" + snake_case)

0915c5e632a089cd4512b6e83af449eba4cff828
Giorgi Amonashvili <109041519+GiorgiAmon@users.noreply.github.com>
2023-03-14 23:36:19 +0400

    Update 2.17-calories.py

--- 2.17-calories.py
+++ 2.17-calories.py
@@ -1,10 +1,12 @@
 def main():
 
     item = input("Fruit :").lower()
-    fruits = {"apple":130, "avocado":50, "banana": 110, "cantaloupe":50,"grapefruit":60, 
+    fruits = {
+    "apple":130, "avocado":50, "banana": 110, "cantaloupe":50,"grapefruit":60, 
     "grapes":90, "kiwifruit":90, "lemon":15, "lime":20, "nectarine":60,
-    "orange":80, "peach":60, "pear":100, "pineapple":50,
-    "plums":70, "strawberries":50, "tangerine":50, "watermelon":8}
+    "orange":80, "peach":60, "pear":100, "pineapple":50,"plums":70, 
+    "sweet cherries" :100, "strawberries":50, "tangerine":50, "watermelon":8
+    }
     if item in fruits:
         print("Calories: " + str(fruits[item]))
     else:

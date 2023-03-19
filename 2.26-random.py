7924a200d8265f0f78b310ab9491039f11f523b7
Giorgi Amonashvili <109041519+GiorgiAmon@users.noreply.github.com>
2023-03-19 21:42:29 +0400

    minor alterations

--- 2.26-random.py
+++ 2.26-random.py
@@ -8,14 +8,12 @@ while completed == False:
         b = random.randint(1,10)
         value = a + b
         guess = input(str(a) + "+"+ str(b) + "=")
-
-        if guess <= 0:
+        if int(guess) <= 0:
             pass
         if int(guess) == value:
             mark += 1
             pass
         else:
             print("EEE")
-            
-    print("Coreect answers " + mark)
+    print("Coreect answers " + str(mark))
     completed =  True

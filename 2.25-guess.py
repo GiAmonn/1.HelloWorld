9cc8d96da03af6c21a56e24739a214491b9902be
Giorgi Amonashvili <109041519+GiorgiAmon@users.noreply.github.com>
2023-03-19 20:40:37 +0400

    added try-except

--- 2.25-guess.py
+++ 2.25-guess.py
@@ -1,16 +1,22 @@
-guessed = False
-number = int(input("Enter a number: "))
-while number< 0: 
-    number = int(input("Enter a number: "))
-    
-while guessed == False:
-    guess = int(input("Guess the number: "))
-    if guess < 0 :
-        pass
-    elif guess < number:
-        print("Too small")
-    elif guess > number:
-        print("Too large")
-    elif guess == number:
-        print("Correct: ")
-        guessed = True
+def main():
+    try:
+        guessed = False
+        number = int(input("Enter a number: "))
+        while number< 0: 
+            number = int(input("Enter a number: "))
+        
+        while guessed == False:
+            guess = int(input("Guess the number: "))
+            if guess < 0 :
+                pass
+            elif guess < number:
+                print("Too small")
+            elif guess > number:
+                print("Too large")
+            elif guess == number:
+                print("Correct: ")
+                guessed = True
+    except ValueError:
+        main()
+
+main()

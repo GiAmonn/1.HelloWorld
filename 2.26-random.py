e75bd6ac01957eb98337cc2d3ee6a0b765746687
Giorgi Amonashvili <109041519+GiorgiAmon@users.noreply.github.com>
2023-03-19 21:36:40 +0400

    initial release

---
+++ 2.26-random.py
@@ -0,0 +1,21 @@
+import random 
+completed = False
+
+while completed == False:
+    mark = 0
+    for i in range(10):
+        a = random.randint(1,10)
+        b = random.randint(1,10)
+        value = a + b
+        guess = input(str(a) + "+"+ str(b) + "=")
+
+        if guess <= 0:
+            pass
+        if int(guess) == value:
+            mark += 1
+            pass
+        else:
+            print("EEE")
+            
+    print("Coreect answers " + mark)
+    completed =  True

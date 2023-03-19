ac81723c49c9ff7f996e72b1cf8cb59afcbb7b42
Giorgi Amonashvili <109041519+GiorgiAmon@users.noreply.github.com>
2023-03-19 20:36:02 +0400

    initial release

---
+++ 2.25-guess.py
@@ -0,0 +1,16 @@
+guessed = False
+number = int(input("Enter a number: "))
+while number< 0: 
+    number = int(input("Enter a number: "))
+    
+while guessed == False:
+    guess = int(input("Guess the number: "))
+    if guess < 0 :
+        pass
+    elif guess < number:
+        print("Too small")
+    elif guess > number:
+        print("Too large")
+    elif guess == number:
+        print("Correct: ")
+        guessed = True

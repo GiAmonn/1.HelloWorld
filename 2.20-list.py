029935e153b307e60f63b23f4919454452f83e5f
Giorgi Amonashvili <109041519+GiorgiAmon@users.noreply.github.com>
2023-03-19 18:23:49 +0400

    Update 2.20-list.py

--- 2.20-list.py
+++ 2.20-list.py
@@ -3,7 +3,7 @@ try:
     num = 1
     while True:
         item = input("choose item: ").title()
-        itemn = str(num) + " "+ item
+        itemn = str(num) + " " + item
         num +=1
         menu.append(itemn)
 

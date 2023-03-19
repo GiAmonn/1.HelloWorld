ba323859d1acd47c544fbbcca7364277be118d1d
Giorgi Amonashvili <109041519+GiorgiAmon@users.noreply.github.com>
2023-03-19 18:06:33 +0400

    added numbering

--- 2.20-list.py
+++ 2.20-list.py
@@ -1,9 +1,11 @@
 try:
     menu = []
-    listn = 0
+    num = 1
     while True:
-        item = input("choose item: ")
-        menu.append(item)
+        item = input("choose item: ").title()
+        itemn = str(num) + " "+ item
+        num +=1
+        menu.append(itemn)
 
 except EOFError:
     print("\n".join(menu))
